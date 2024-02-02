67d38cc2a77b44fb833d1423ba520d7b9e6ba8bb
SeungYeon <orrhrr@gmail.com>
2024-02-02 21:16:42 +0900

    feat: 신간 페이지 레이아웃 제작

--- src/pages/bestseller/index.tsx
+++ src/pages/bestseller/index.tsx
@@ -1,16 +1,18 @@
+import BookOverViewCardList from '@/components/card/bookOverviewCard/bookOverViewCardList';
 import Header from '@/components/header';
 import BestSellerPageLayout from '@/layouts/bestSellerLayout';
+import { bookOverviewsMock } from '../api/mock/bestSellerMock';
 
-function BestSeller() {
+function BestSellerPage() {
   return (
     <div>
       <BestSellerPageLayout
         header={<Header isLoggedIn={true} />}
         // sideBar={ }
-        // main={ }
+        main={<BookOverViewCardList bookData={bookOverviewsMock} />}
       />
     </div>
   );
 }
 
-export default BestSeller;
+export default BestSellerPage;
--- src/pages/newest/index.tsx
+++ src/pages/newest/index.tsx
@@ -1,3 +1,18 @@
-export default function Newest() {
-  return <div>신간</div>;
+import BookOverViewCardList from '@/components/card/bookOverviewCard/bookOverViewCardList';
+import Header from '@/components/header';
+import BestSellerPageLayout from '@/layouts/bestSellerLayout';
+import { bookOverviewsMock } from '../api/mock/bestSellerMock';
+
+function NewestPage() {
+  return (
+    <div>
+      <BestSellerPageLayout
+        header={<Header isLoggedIn={true} />}
+        // sideBar={ }
+        main={<BookOverViewCardList bookData={bookOverviewsMock} />}
+      />
+    </div>
+  );
 }
+
+export default NewestPage;
