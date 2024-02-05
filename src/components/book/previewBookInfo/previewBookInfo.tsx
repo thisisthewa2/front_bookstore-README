0962e838b2077b0efae8c8742080693b50f9e397
SeungYeon <orrhrr@gmail.com>
2024-02-05 18:10:34 +0900

    style: IMAGE_SIZE 상수값 constants폴더로 이동

--- src/components/book/previewBookInfo/previewBookInfo.tsx
+++ src/components/book/previewBookInfo/previewBookInfo.tsx
@@ -5,6 +5,7 @@ import { THOUSAND_UNIT } from 'src/constants/price';
 import BookLabelGrayIcon from '@/public/icons/BookLabelGrayIcon.svg';
 import BookLabelGreenIcon from '@/public/icons/BookLabelIGreenIcon.svg';
 import { PreviewBookInfoProps } from '@/types/previewBookInfoType';
+import { IMAGE_SIZE } from 'src/constants/previewBookSize';
 
 function PreviewBookInfo({
   image,
@@ -21,29 +22,7 @@ function PreviewBookInfo({
   const [isLabelMove, setIsLabelMove] = useState(false);
   const [imageLoaded, setImageLoaded] = useState(false);
   const [rawImageSize, setRawImageSize] = useState({ width: 0, height: 0 });
-  const IMAGE_SIZE = {
-    lg: {
-      pc: 'w-192 h-291',
-      tablet: 'tablet:w-157 tablet:h-237',
-      mobile: 'mobile:w-160 mobile:h-242',
-      widthOnly: 'w-192 tablet:w-157 mobile:w-160',
-      heightNumber: { pc: 291, tablet: 237, mobile: 242 },
-    },
-    md: {
-      pc: 'w-163 h-246',
-      tablet: 'tablet:w-122 tablet:h-184',
-      mobile: 'mobile:w-142 mobile:h-215',
-      widthOnly: 'w-163 tablet:w-122 mobile:w-142',
-      heightNumber: { pc: 246, tablet: 184, mobile: 215 },
-    },
-    sm: {
-      pc: 'w-112 h-170',
-      tablet: 'tablet:w-112 tablet:h-170',
-      mobile: 'mobile:w-93 mobile:h-141',
-      widthOnly: 'w-112 tablet:w-112 mobile:w-93',
-      heightNumber: { pc: 170, tablet: 170, mobile: 141 },
-    },
-  };
+
   const imageSize = IMAGE_SIZE[size];
   const STYLE = {
     img: `${IMAGE_SIZE[size].pc} ${IMAGE_SIZE[size].tablet} ${IMAGE_SIZE[size].mobile}`,
