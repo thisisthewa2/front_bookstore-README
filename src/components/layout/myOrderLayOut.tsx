0373bb2282cd17a506b22e7e87a0770aff1a0e3a
SeungYeon <orrhrr@gmail.com>
2024-02-07 17:50:43 +0900

    style: test/orderPage 추가

--- src/components/layout/myOrderLayOut.tsx
+++ src/components/layout/myOrderLayOut.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode } from 'react';
 
 interface MyOrderPageLayoutProps {
   header: ReactNode;
@@ -7,16 +7,24 @@ interface MyOrderPageLayoutProps {
   main: ReactNode;
 }
 
-function MyOrderPageLayout({header, overview, orderDate, main} : MyOrderPageLayoutProps) {
-  return (<>
-    <div role="container" className="basis-0pxr flex w-full shrink-0 grow flex-col overflow-hidden">
-      <div role="header">{header}</div>
-      <div role="overview">{overview}</div>
-      <div role="order-date">{orderDate}</div>
-      <div role="content">{main}</div>
-      
-  </div>
-  </>)
+function MyOrderPageLayout({
+  header,
+  overview,
+  orderDate,
+  main,
+}: MyOrderPageLayoutProps) {
+  return (
+    <>
+      <div
+        role="container"
+        className="basis-0pxr flex w-full shrink-0 grow flex-col overflow-hidden">
+        <div role="header">{header}</div>
+        <div role="overview">{overview}</div>
+        <div role="order-date">{orderDate}</div>
+        <div role="content">{main}</div>
+      </div>
+    </>
+  );
 }
 
 export default MyOrderPageLayout;
--- src/pages/mypage/order/index.tsx
+++ src/pages/mypage/order/index.tsx
@@ -1,13 +1,16 @@
-import BookOrderCard from "@/components/card/bookOrderCard/bookOrderCard";
-import OrderDate from "@/components/container/orderDate/orderDate";
-import OrderOverView from "@/components/container/orderDate/orderOverView";
-import Header from "@/components/header";
-import MyOrderPageLayout from "@/components/layout/myOrderLayOut";
+import BookOrderCard from '@/components/card/bookOrderCard/bookOrderCard';
+import BookOrderCardList from '@/components/card/bookOrderCard/bookOrderCardList';
+import OrderDate from '@/components/container/orderDate/orderDate';
+import OrderOverView from '@/components/container/orderDate/orderOverView';
+import Header from '@/components/header';
+import MyOrderPageLayout from '@/components/layout/myOrderLayOut';
+import { bookOrderTestData } from '@/pages/api/mock/bookOrderMock';
+const { orderData } = bookOrderTestData;
 
-function MyOrderPage() {
-  return (<section>
-    {/* <MyOrderPageLayout header={<Header isLoggedIn={true}} overview={<OrderOverView />} orderDate={<OrderDate />} main={<BookOrderCardList />} /> */}
-  </section>)
-  
-}
-export default MyOrderPage;
+// function MyOrderPage() {
+//   return (
+//  <MyOrderPageLayout header={<Header isLoggedIn/>} orderDate={<OrderDate />} overview={<OrderOverView />} main={<BookOrderCardList />} />
+// )
+
+// }
+// export default MyOrderPage;
